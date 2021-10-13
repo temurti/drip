9d046848377621bc5fb57aaa25ecc293427290d1
Joel Jesudason <62968241+JoelJesudason@users.noreply.github.com>
2021-10-13 14:11:38 -0400

    NFT logic polishing

--- nft_test_interface/client/src/rinkeby_config.js
+++ nft_test_interface/client/src/rinkeby_config.js
@@ -1,3 +1,3 @@
-export const appAddress = "0xB62C96A13b1FD2A320279280F3fA121e5ED4D315";
+export const appAddress = "0xd1BC5b3b3c771c2143299dd357E3917c22e4e918";
 export const fUSDCxAddress = "0x0F1D7C55A2B133E000eA10EeC03c774e0d6796e8"
 export const fDAIxAddress = "0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90"
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,14 +8,14 @@ async function main() {
     const TradeableFlow = await ethers.getContractFactory("TradeableFlow");
 
     const token = await TradeableFlow.deploy(            
-        deployer.address,
-        "TradeableFlow v3",
-        "TF",
+        "0xc41876DAB61De145093b6aA87417326B24Ae4ECD",      // program owner address
+        "Uniwhales Drip Cashflow NFT",                        
+        "DRIP-UWL",
+        "base-link",                                       // base URI
         "0xeD5B5b32110c3Ded02a07c8b8e97513FAfb883B6",      // host
         "0xF4C5310E51F6079F601a5fb7120bC72a70b96e2A",      // cfa
-        // "0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90",   // SuperToken accepted by app (starting off with fDAIx)
-        "0xbDfb61f061250a1f6A9e184B7B0EE8d7d4f83cfC",      // ERC20Restrict token
-        200000000000                                       // Affiliate Portion (20%));
+        200000000000,                                      // Affiliate Portion (20%)
+        "0xc41876DAB61De145093b6aA87417326B24Ae4ECD"       // Drip owner address
     );
 
     console.log("Contract address:", token.address);
@@ -27,3 +27,6 @@ async function main() {
       console.error(error);
       process.exit(1);
 });
+
+// npx hardhat run scripts/deploy.js --network rinkeby
+// fake UWL: 0xbDfb61f061250a1f6A9e184B7B0EE8d7d4f83cfC
