b7f8a61ee8c3857180c4e426d14db0ed1daab70e
Joel Jesudason <62968241+JoelJesudason@users.noreply.github.com>
2021-11-05 15:39:31 -0400

    adjusted deploy.js for constructor changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,14 +8,15 @@ async function main() {
     const TradeableFlow = await ethers.getContractFactory("TradeableFlow");
 
     const token = await TradeableFlow.deploy(            
-        "0xc41876DAB61De145093b6aA87417326B24Ae4ECD",      // program owner address
+        "0xc41876DAB61De145093b6aA87417326B24Ae4ECD",      // program owner address (get's the revenue cash flow)
+        "0xc41876DAB61De145093b6aA87417326B24Ae4ECD",      // Drip owner address (Drip admin, can lock program if not being paid)
         "Uniwhales Drip Cashflow NFT",                        
         "DRIP-UWL",
         "base-link",                                       // base URI
         "0xeD5B5b32110c3Ded02a07c8b8e97513FAfb883B6",      // host
         "0xF4C5310E51F6079F601a5fb7120bC72a70b96e2A",      // cfa
         200000000000,                                      // Affiliate Portion (20%)
-        "0xc41876DAB61De145093b6aA87417326B24Ae4ECD"       // Drip owner address
+        ""                                                 // Registration key (will not be blank for mainnet deployment)
     );
 
     console.log("Contract address:", token.address);
